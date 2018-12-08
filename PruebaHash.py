9af67491e01818d0bc4beb2ec0ed7ea280e598c8
Alan Guzman <alanosvaldo88@gmail.com>
2018-12-07 23:27:08 -0600

    Commit. Funciones hash y prueba de funcionamiento

---
+++ PruebaHash.py
@@ -0,0 +1,41 @@
+'''
+Created on 4 dic. 2018
+
+@author: aochi
+'''
+
+class hash_table:
+    def __init__(self):
+        self.table = (None) *127
+    
+    def Hash_func(self, value):
+        key=0
+        for i in range(0,len(value)):
+            key += ord(value[i])
+        return key % 127
+    
+    def Insert(self, value):
+        hash = self.Hash_func(value)
+        if self.table[hash]is None:
+            self.table[hash] = value
+    
+    def Search(self, value):
+        hash = self.Hash_func(value)
+        if self.table[hash]is None:
+            return None
+        else:
+            print("Se encontró el elemento en: ")
+            return id(self.table[hash])
+    
+    
+    
+    def Remove(self, value):
+        
+        
+
+H = hash_table()
+H.Insert("ALV")
+H.Insert("Bou")
+H.Insert("Col")
+
+print(H.Search("Bou"))        
